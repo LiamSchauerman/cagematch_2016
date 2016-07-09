026317580f9327a1fe0da65804116c4780b304ef
Liam Schauerman <ljschauerman@gmail.com>
2016-07-09 13:54:06 -0700

    init

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import Header from './Header'
-import Home from './Home';
-import NewsWrapper from './NewsWrapper';
 import styles from '../styles/main.scss';
 
 class Main extends Component {
   render() {
     return (
-      <div class="app">
+      <div className="app">
         <Header />
-        <div className="contentWrapper">
-          <Home />
-          <NewsWrapper />
-
-        </div>
+        <div>Hello World</div>
       </div>
     );
   }
